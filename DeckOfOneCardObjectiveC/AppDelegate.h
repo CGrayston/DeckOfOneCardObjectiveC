81c147eb34e3029a1cafd2a188bbfb846220bc1c
Chris Grayston <chris.grayston95@gmail.com>
2019-02-12 13:42:45 -0700

    Initial Commit

---
+++ DeckOfOneCardObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Chris Grayston on 2/12/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckOfOneCardObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckOfOneCardObjectiveC
+//
+//  Created by Chris Grayston on 2/12/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
